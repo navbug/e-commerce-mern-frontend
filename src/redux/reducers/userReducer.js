1f39a5712629ec0cfbf9e62a0b656c0f959ef509
Naveen Bugalia <51168623+navbug@users.noreply.github.com>
2024-06-12 22:09:41 +0530

    Update userReducer.js

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -4,18 +4,14 @@ export const userReducer = createSlice({
   name: 'user',
   initialState: {
     user: null,
-    wishlist: [],
   },
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
     },
-    setWishlist: (state, action) => {
-      state.wishlist = action.payload;
-    },
   },
 });
 
-export const { setUser, setWishlist } = userReducer.actions;
+export const { setUser } = userReducer.actions;
 
 export default userReducer.reducer;
