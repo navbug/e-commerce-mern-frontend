74c727e5414d8a34bf12fd8ae8413e95af92052d
Naveen Bugalia <naveenbugalia512@gmail.com>
2025-10-22 20:55:46 +0530

    updated

--- src/pages/Admin/ManageCustomers.jsx
+++ src/pages/Admin/ManageCustomers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { deleteUser, getAllUsers } from "../../api";
 import { adminIds } from "../../utils/helpers";
 import { toast } from "react-toastify";
