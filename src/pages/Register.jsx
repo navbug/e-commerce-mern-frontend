922b9fe15e73068c45e4c9845c39f1d5d40a5df5
Naveen Bugalia <naveenbugalia512@gmail.com>
2025-10-22 20:58:42 +0530

    updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { ClipLoader } from "react-spinners";
 import { toast } from "react-toastify";
